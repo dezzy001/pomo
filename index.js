ab97fbc27359d2f5b47b01a1a008c3831bfabf8e
dezzy001 <dezzy001@gmail.com>
2018-08-15 21:08:31 +1000

    added firebase and users

---
+++ index.js
@@ -0,0 +1,89 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+    document.getElementById("create_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+function create_div(){
+  document.getElementById("create_div").style.display = "block";
+  document.getElementById("login_div").style.display = "none";
+}
+function back(){
+  document.getElementById("create_div").style.display = "none";
+  document.getElementById("login_div").style.display = "block";
+}
+
+
+function login(){
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function create(){
+  var userEmail = document.getElementById("email_field_create").value;
+  var userPass = document.getElementById("password_field_create").value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+//for testing only###############################################
+function skip(){
+  //test email
+  var userEmail = "dezzy001@gmail.com";
+  var userPass = "qwerty123";
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
